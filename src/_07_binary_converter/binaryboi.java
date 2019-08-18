be82364cda34fe6cf8eeacd06353dbb5648fac78
League Student <league@iMac-37.attlocal.net>
2019-08-18 14:28:28 -0700

    oopsie
    I did an oopsie

--- src/_07_binary_converter/binaryboi.java
+++ src/_07_binary_converter/binaryboi.java
@@ -1,5 +1,17 @@
 package _07_binary_converter;
 
-public class binaryboi {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class binaryboi {
+private void mian() {
+	// TODO Auto-generated method stub
+JFrame JF = new JFrame();
+JF.pack();
+JPanel JP = new JPanel();
+JP.add(JF);
+	
+	
+	
+}
 }
